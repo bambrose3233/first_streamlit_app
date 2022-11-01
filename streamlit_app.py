25dbc99aba52362a15adca2f85bc64fcff558baa
bambrose3233 <117193847+bambrose3233@users.noreply.github.com>
2022-11-01 10:59:06 -0500

    Update streamlit_app.py
    Added additional menu options and fixed typo in header

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,9 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 
-streamlit.header('VBreakfast Menu')
+streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
+
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+
+streamlit.text('Hard-boiled Free-Range Egg')
